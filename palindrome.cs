80f16c182823bb9b0f07b8e7650a7309372a7ccb
shanmu27 <shanmu2709@gmail.com>
2016-03-16 11:19:41 +0530

    Added files via upload

---
+++ palindrome.cs
@@ -0,0 +1,71 @@
+using System;
+
+class Program
+{
+    /// <summary>
+    /// Determines whether the string is a palindrome.
+    /// </summary>
+    public static bool IsPalindrome(string value)
+    {
+	int min = 0;
+	int max = value.Length - 1;
+	while (true)
+	{
+	    if (min > max)
+	    {
+		return true;
+	    }
+	    char a = value[min];
+	    char b = value[max];
+	    if (char.ToLower(a) != char.ToLower(b))
+	    {
+		return false;
+	    }
+	    min++;
+	    max--;
+	}
+    }
+
+    static void Main()
+    {
+	string[] array =
+	{
+	    "civic",
+	    "deified",
+	    "deleveled",
+	    "devoved",
+	    "dewed",
+	    "Hannah",
+	    "kayak",
+	    "level",
+	    "madam",
+	    "racecar",
+	    "radar",
+	    "redder",
+	    "refer",
+	    "repaper",
+	    "reviver",
+	    "rotator",
+	    "rotor",
+	    "sagas",
+	    "solos",
+	    "sexes",
+	    "stats",
+	    "tenet",
+
+	    "Dot",
+	    "Net",
+	    "Perls",
+	    "Is",
+	    "Not",
+	    "A",
+	    "Palindrome",
+	    ""
+	};
+
+	foreach (string value in array)
+	{
+	    Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+	}
+    }
+}
